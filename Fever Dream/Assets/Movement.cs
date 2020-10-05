41d96ab5a64fe74eba9c677b47bb9a15b5290ee2
Day-Ham <37504037+Day-Ham@users.noreply.github.com>
2020-10-05 15:06:20 +0800

    New Project

---
+++ Fever Dream/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
+}
